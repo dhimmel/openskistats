e580ec8eb185ef6d95fe6d4ad23d6a8994b032a1
let20 <trang.le@bms.com>
2024-12-22 16:14:18 -0500

    add us map, refine

--- website/skirolly/02.plot.R
+++ website/skirolly/02.plot.R
@@ -2,31 +2,14 @@ library(arrow)
 library(dplyr)
 library(ggplot2)
 library(yaml)
+library(patchwork)
+library(cowplot)
 
-x1 <- -72.095
-y1 <- 43.7775
-x2 <- -72.101
-y2 <- 43.7900
-m <- (y2 - y1) / (x2 - x1)
-b <- y1 - m * x1
-
-data_dir <- "../images/data"
+img_data_dir <- "../images/data"
 img_dir <- "../images"
-dart_url <- "https://github.com/user-attachments/assets/1a02ca26-7034-4d87-bc0c-01f6bed997f7"
-# download.file(dart_url, destfile = file.path(img_dir, "dartmouth.png"))
-# dartmouth_img <- png::readPNG(file.path(img_dir, "dartmouth.png"), native = TRUE)
-bearings_ls <- readRDS(file.path(data_dir, "bearings_48_ls.rds"))
-dartmouth_segs <- read_parquet(file.path(data_dir, "dartmouth_segs.parquet"))
-hemi <- read_parquet(file.path(data_dir, "hemisphere_roses.parquet")) |>
-  tidyr::unnest(bearings) 
-
-dart <- read_parquet(file.path(data_dir, "dartmouth_runs.parquet")) |>
-  group_by(run_id) |>
-  mutate(winslow = (m * longitude) + b < latitude) |>
-  arrange(index)
-
 colors <- c("#f07178", "#004B59", "#FFC857", "#36B37E", "#FF8C42", "#F4F1E9", "#8A9393", "#2A2D34")
 
+## Plot functions ------------------------------------------------
 bg_transparent <- function() {
   theme_minimal() +
     theme(
@@ -37,18 +20,19 @@ bg_transparent <- function() {
       legend.position = "none",
     )
 }
-plot_rose <- function(dat, ski_area_name, size_title = 24, size_x = 20, highlight = FALSE, labels = NULL, type = NULL, hemi = NULL) {
+plot_rose <- function(dat, ski_area_name, size_title = 24, size_x = 20, highlight = FALSE, labels = NULL, type = NULL, hemi = NULL, ngr = 32, vert = 0) {
+  y_break <- max(dat$bin_count)
   p <- dat |>
     ggplot() +
     aes(x = bin_center, y = bin_count) +
-    coord_radial(start = -pi / 32, expand = FALSE) +
+    coord_radial(start = -pi / ngr, expand = FALSE) +
     # coord_polar(start = -pi / 32) +
     scale_x_continuous(
       breaks = seq(0, 270, 90),
       labels = labels
     ) +
     scale_y_sqrt( # scaled by area
-      breaks = max(dat$bin_count)
+      breaks = y_break
     ) +
     labs(title = ski_area_name) +
     bg_transparent() +
@@ -56,26 +40,105 @@ plot_rose <- function(dat, ski_area_name, size_title = 24, size_x = 20, highligh
       panel.grid = element_line(color = "grey60"),
       axis.text.x = element_text(size = size_x, color = "#EBEBEB"),
       plot.title = element_text(hjust = 0.5, size = size_title, color = "#EBEBEB")
-    )
+    ) +
+    NULL
   if (!is.null(type)) {
-    p <- p + geom_col(fill = "#f07178") +
+    p <- p + 
       theme(
         panel.grid.major.x = element_blank(),
         panel.grid.minor.x = element_blank(),
+        panel.grid.major.y = element_line(linewidth = vert)
       )
-  } else if (!is.null(hemi)){
+    if (type == "all"){
+      p <- p +
+        geom_col(fill = "#f07178", width = 360 / ngr)
+    }
+    if (type == "us") {
+      p <- p +
+        geom_col(fill = "#f07178", width = 360 / ngr, color = "#EBEBEB", linewidth = 0.2) +
+        annotate(
+          geom = "label",
+          label = ski_area_name,
+          x = 180, y = max(dat$bin_count),
+          hjust = 0.5,
+          alpha = 0.5,
+          fill = "#002B36",
+          size = size_title,
+          family = "Futura",
+          color = "#EBEBEB"
+        ) +
+        theme(
+          # plot.margin=grid::unit(c(-35,-35, -35, -35), "mm"),
+          panel.background = element_rect(fill = "#002B36"),
+          plot.title = element_blank()
+        )
+    }
+  } else if (!is.null(hemi)) {
     p <- p +
-      geom_col(color = "#EBEBEB", aes(fill = color)) +
+      geom_col(color = "#EBEBEB", width = 360 / ngr, aes(fill = color)) +
       scale_fill_manual(values = hemi)
   } else if (!highlight) {
-    p <- p + geom_col(color = "#EBEBEB", fill = "#f07178")
+    p <- p + geom_col(color = "#EBEBEB", width = 360 / ngr, fill = "#f07178")
   } else {
     p <- p +
-      geom_col(color = "#EBEBEB", aes(fill = color)) +
+      geom_col(color = "#EBEBEB", width = 360 / ngr, aes(fill = color)) +
       scale_fill_identity()
   }
   p
 }
+
+rose_empty <- function(a, size_label) {
+  data.frame(x = c(0, 360), y = c(1, 1)) |>
+    ggplot() +
+    geom_col(fill = "#f07178", alpha = 0) +
+    aes(x = x, y = y) +
+    coord_radial() +
+    scale_y_sqrt( # scaled by area
+      breaks = 1
+    ) +
+    annotate(
+      geom = "label",
+      label = a,
+      x = 180,
+      y = 1,
+      hjust = 0.5,
+      alpha = 0.5,
+      fill = "#002B36",
+      size = size_label,
+      family = "Futura",
+      color = "#EBEBEB"
+    ) +
+    bg_transparent() +
+    theme(
+      panel.background = element_rect(fill = "#002B36"),
+      plot.background = element_blank(),
+      panel.grid.major.y = element_blank(),
+      panel.grid.major.x = element_blank(),
+      panel.grid.minor.x = element_blank(),
+    )
+}
+
+## Data ------------------------------------------------
+x1 <- -72.095
+y1 <- 43.7775
+x2 <- -72.101
+y2 <- 43.7900
+m <- (y2 - y1) / (x2 - x1)
+b <- y1 - m * x1
+
+dart_url <- "https://github.com/user-attachments/assets/1a02ca26-7034-4d87-bc0c-01f6bed997f7"
+# download.file(dart_url, destfile = file.path(img_dir, "dartmouth.png"))
+# dartmouth_img <- png::readPNG(file.path(img_dir, "dartmouth.png"), native = TRUE)
+bearings_ls <- readRDS(file.path(img_data_dir, "bearings_48_ls.rds"))
+dartmouth_segs <- read_parquet(file.path(img_data_dir, "dartmouth_segs.parquet"))
+hemi <- read_parquet(file.path(img_data_dir, "hemisphere_roses.parquet")) |>
+  tidyr::unnest(bearings) 
+
+dart <- read_parquet(file.path(img_data_dir, "dartmouth_runs.parquet")) |>
+  group_by(run_id) |>
+  mutate(winslow = (m * longitude) + b < latitude) |>
+  arrange(index)
+
 dartmouth <- bearings_ls[["Dartmouth Skiway"]]
 # whaleback <- bearings_ls[["Whaleback Mountain"]]
 # killington <- bearings_ls[["Killington Resort"]]
@@ -225,6 +288,7 @@ all_roses <- cowplot::plot_grid(
     plot_rose,
     size_x = 7,
     size_title = 9,
+    vert = 0.1,
     type = "all"
   )
 )
@@ -247,4 +311,150 @@ ggsave(
   file.path(img_dir, "hemisphere.png"), 
   cowplot::plot_grid(north, south, ncol = 2),
   width = 12, height = 6, dpi = 300
+)
+
+## Region -----------------------------------------------------------
+ngroups_us <- 16
+my_font <- "Futura"
+max_thickness <- 6
+min_thickness <- 0.2
+size_label <- 12
+water_mark <- "OpenSkiStats.org\nLicense: CC BY 4.0"
+region_raw <- arrow::read_parquet(file.path(img_data_dir, "region_roses.parquet"))
+verts <- region_raw |>
+  select(region, combined_vertical) |>
+  mutate(thickness = sqrt(combined_vertical / max(combined_vertical)) * max_thickness + min_thickness, .keep = "unused") |>
+  tibble::deframe()
+n_ski_areas <- sum(region_raw$ski_areas_count)
+
+region <- region_raw |>
+  dplyr::select(region, bearings) |>
+  tibble::deframe() |>
+  lapply(
+    \(x) dplyr::filter(x, num_bins == ngroups_us) |>
+      dplyr::mutate(
+        color = dplyr::if_else(bin_index == 2, "#f07178", "#004B59"),
+      )
+  )
+
+layout <- c(
+  area(1, 1),
+  area(1, 11),
+  area(2, 10, 2, 10), area(2, 11),
+  area(3, 1), area(3, 2), area(3, 3), area(3, 4), area(3, 5),
+  area(3, 7), area(3, 9), area(3, 10), area(3, 11),
+  area(4, 1), area(4, 2), area(4, 3), area(4, 4), area(4, 5),
+  area(4, 6), area(4, 7), area(4, 8), area(4, 9), area(4, 10),
+  area(5, 1), area(5, 2), area(5, 3), area(5, 4), area(5, 5),
+  area(5, 6), area(5, 7), area(5, 8), area(5, 9), area(5, 10),
+  area(6, 2), area(6, 3), area(6, 4), area(6, 5), area(6, 6),
+  area(6, 7), area(6, 8), area(6, 9), area(6, 10),
+  area(7, 3), area(7, 4), area(7, 5), area(7, 6), area(7, 7), area(7, 8),
+  area(8, 1), area(8, 3), area(8, 9)
+)
+
+state_names <- c(
+  "AK",
+  "ME", "VT", "NH", "WA", "ID", "MT", "ND", "MN", "MI", "NY", "MA", "RI",
+  "OR", "UT", "WY", "SD", "IA", "WI", "OH", "PA", "NJ", "CT",
+  "CA", "NV", "CO", "NE", "IL", "IN", "WV", "VA", "MD", "DE",
+  "AZ", "NM", "KS", "MO", "KY", "TN", "SC", "NC", "DC",
+  "OK", "LA", "AR", "MS", "AL", "GA",
+  "HI", "TX", "FL"
+)
+
+state_map <- setNames(state.abb, state.name)
+all_regions <- list(region, state_map[names(region)], vert = verts) |> 
+  purrr::pmap(
+    plot_rose,
+    size_title = size_label,
+    ngr = ngroups_us,
+    type = "us"
+  )
+all_regions[[1]]
+names(all_regions) <- state_map[names(region)]
+
+state_plots <- lapply(state_names, rose_empty, size_label = size_label) |>
+  setNames(state_names)
+
+skiable_states <- names(all_regions)
+for (i in skiable_states) {
+  state_plots[[i]] <- all_regions[[i]]
+}
+state_plots <- state_plots[state_names]
+# state_plots[[1]]
+map_plots <- wrap_plots(state_plots, design = layout) &
+  plot_annotation(theme = theme(
+    panel.background = element_rect(fill = "#004B59", colour = NA),
+    plot.background = element_rect(fill = "#004B59", colour = NA)
+  ))
+
+# sysfonts::font_add_google("Nunito Sans")
+# showtext::showtext_auto()
+
+title <-
+  ggplot() +
+  theme_void() +
+  coord_cartesian(ylim = c(-2, 0.7)) +
+  annotate(
+    "text",
+    x = 0, y = 0,
+    label = "Which way do you ski?", size = 32,
+    color = "#EBEBEB",
+    family = my_font
+  ) +
+  annotate(
+    "text",
+    x = 0, y = -0.9,
+    label = sprintf("Orientations of %s US ski areas", n_ski_areas),
+    size = 18,
+    color = "#EBEBEB",
+    family = my_font
+  )
+
+layout_title <- c(area(1, 3, 2, 9), layout)
+map_plots <- c(list(title = title), state_plots) |>
+  wrap_plots(design = layout_title) &
+  plot_annotation(theme = theme(
+    panel.background = element_rect(fill = "#004B59", colour = NA),
+    plot.background = element_rect(fill = "#004B59", colour = NA)
+  ))
+
+to_publish <- ggdraw(map_plots) +
+  draw_label(
+    x = 0.985,
+    y = 0.043,
+    size = 28,
+    hjust = 1,
+    color = "grey60",
+    fontfamily = my_font,
+    lineheight = 1.6,
+    label = water_mark
+  ) +
+  draw_label(
+    x = 0.165,
+    y = 0.78,
+    size = 28,
+    hjust = 1,
+    color = "grey60",
+    fontfamily = my_font,
+    label = "border proportional to\ncombined vertical drop"
+  ) +
+  draw_line(
+    x = c(0.185, 0.21),
+    y = c(0.768, 0.726),
+    color = "grey60", size = 2
+  ) +
+  draw_line(
+    x = c(0.17, 0.185),
+    y = c(0.768, 0.768),
+    color = "grey60", size = 2
+  ) +
+  NULL
+
+ggsave(
+  file.path(img_dir, "us_roses.png"),
+  to_publish,
+  width = 11 * 3, height = 8 * 3, units = "in",
+  dpi = 300
 )
