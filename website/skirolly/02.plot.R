44aea487f72fdbbdb3dcaf6b056dd306272edcbd
let20 <trang.le@bms.com>
2024-12-23 12:10:48 -0500

    sysfonts, showtext, refine

--- website/skirolly/02.plot.R
+++ website/skirolly/02.plot.R
@@ -8,7 +8,45 @@ library(cowplot)
 img_data_dir <- "../images/data"
 img_dir <- "../images"
 colors <- c("#f07178", "#004B59", "#FFC857", "#36B37E", "#FF8C42", "#F4F1E9", "#8A9393", "#2A2D34")
-my_font <- "Helvetica"
+
+rose_pink <- "#f07178"
+text_teal <- "#004B59"
+text_light <- "#EBEBEB"
+dark_green <- "#002B36"
+
+my_font <- "Alegreya Sans"
+sysfonts::font_add_google(my_font)
+showtext::showtext_auto()
+
+# for US map:
+
+ngroups_us <- 16
+max_thickness <- 6
+min_thickness <- 0.2
+size_label <- 12
+
+dark_pal <- list(
+  title_color = text_light,
+  anno_color = "grey80",
+  circle_color = "grey60",
+  canvas = text_teal,
+  circle_fill = dark_green,
+  size_label = size_label,
+  my_font = my_font
+)
+
+light_pal <- list(
+  title_color = "grey10",
+  anno_color = "grey30",
+  circle_color = "grey50",
+  canvas = "white",
+  circle_fill = "grey90",
+  size_label = size_label,
+  my_font = my_font
+)
+
+my_aesthetics <- list(light = light_pal, dark = dark_pal)
+
 
 ## Plot functions ------------------------------------------------
 bg_transparent <- function() {
@@ -21,101 +59,74 @@ bg_transparent <- function() {
       legend.position = "none",
     )
 }
-plot_rose <- function(dat, ski_area_name, size_title = 24, size_x = 20, highlight = FALSE, labels = NULL, type = NULL, hemi = NULL, ngr = 32, vert = 0) {
+plot_rose <- function(
+    dat, ski_area_name, size_title = 24, size_x = 20, 
+    highlight = FALSE, labels = NULL, type = NULL, 
+    hemi = NULL, ngr = 32, vert = 0, aesthetics = NULL,
+    empty_rose = FALSE
+  ) {
   y_break <- max(dat$bin_count)
   p <- dat |>
     ggplot() +
     aes(x = bin_center, y = bin_count) +
     coord_radial(start = -pi / ngr, expand = FALSE) +
     # coord_polar(start = -pi / 32) +
-    scale_x_continuous(
-      breaks = seq(0, 270, 90),
-      labels = labels
-    ) +
-    scale_y_sqrt( # scaled by area
-      breaks = y_break
-    ) +
+    scale_x_continuous(breaks = seq(0, 270, 90), labels = labels) +
+    scale_y_sqrt(breaks = y_break) + # scaled by area
     labs(title = ski_area_name) +
     bg_transparent() +
     theme(
       panel.grid = element_line(color = "grey60"),
-      axis.text.x = element_text(size = size_x, color = "#EBEBEB"),
-      plot.title = element_text(hjust = 0.5, size = size_title, color = "#EBEBEB")
-    ) +
-    NULL
+      axis.text.x = element_text(size = size_x, color = text_light),
+      plot.title = element_text(hjust = 0.5, size = size_title, color = text_light)
+    ) 
+  
   if (!is.null(type)) {
     p <- p + 
       theme(
         panel.grid.major.x = element_blank(),
         panel.grid.minor.x = element_blank(),
-        panel.grid.major.y = element_line(linewidth = vert)
+        panel.grid.major.y = element_line(linewidth = vert, color = aesthetics$circle_color)
       )
-    if (type == "all"){
-      p <- p +
-        geom_col(fill = "#f07178", width = 360 / ngr)
+    if (type == "all") {
+      p <- p + geom_col(fill = rose_pink)
     }
     if (type == "us") {
       p <- p +
-        geom_col(fill = "#f07178", width = 360 / ngr, color = "#EBEBEB", linewidth = 0.2) +
+        {if (!empty_rose) geom_col(fill = rose_pink, width = 360 / ngr, color = text_light, linewidth = 0.2) else geom_col(fill = NA)} +
         annotate(
           geom = "label",
           label = ski_area_name,
           x = 180, y = max(dat$bin_count),
           hjust = 0.5,
-          alpha = 0.5,
-          fill = "#002B36",
-          size = size_title,
-          family = my_font,
-          color = "#EBEBEB"
+          alpha = 0.8,
+          fill = aesthetics$circle_fill,
+          size = aesthetics$size_label,
+          family = aesthetics$my_font,
+          color = aesthetics$title_color
         ) +
         theme(
           # plot.margin=grid::unit(c(-35,-35, -35, -35), "mm"),
-          panel.background = element_rect(fill = "#002B36"),
+          panel.background = element_rect(fill = aesthetics$circle_fill),
           plot.title = element_blank()
         )
     }
-  } else if (!is.null(hemi)) {
-    p <- p +
-      geom_col(color = "#EBEBEB", width = 360 / ngr, aes(fill = color)) +
-      scale_fill_manual(values = hemi)
   } else if (!highlight) {
-    p <- p + geom_col(color = "#EBEBEB", width = 360 / ngr, fill = "#f07178")
+    p <- p + geom_col(color = text_light, width = 360 / ngr, fill = rose_pink)
   } else {
     p <- p +
-      geom_col(color = "#EBEBEB", width = 360 / ngr, aes(fill = color)) +
+      geom_col(color = text_light, width = 360 / ngr, aes(fill = color)) +
       scale_fill_identity()
   }
   p
 }
 
-rose_empty <- function(a, size_label) {
-  data.frame(x = c(0, 360), y = c(1, 1)) |>
-    ggplot() +
-    geom_col(fill = "#f07178", alpha = 0) +
-    aes(x = x, y = y) +
-    coord_radial() +
-    scale_y_sqrt( # scaled by area
-      breaks = 1
-    ) +
-    annotate(
-      geom = "label",
-      label = a,
-      x = 180,
-      y = 1,
-      hjust = 0.5,
-      alpha = 0.5,
-      fill = "#002B36",
-      size = size_label,
-      family = my_font,
-      color = "#EBEBEB"
-    ) +
-    bg_transparent() +
+rose_empty <- function(ski_area_name, aesthetics) {
+  dat <- data.frame(bin_center = c(0, 360), bin_count = c(1, 1))
+  plot_rose(dat, ski_area_name, type = "us", aesthetics = aesthetics, empty_rose = TRUE) +
     theme(
-      panel.background = element_rect(fill = "#002B36"),
       plot.background = element_blank(),
       panel.grid.major.y = element_blank(),
-      panel.grid.major.x = element_blank(),
-      panel.grid.minor.x = element_blank(),
     )
 }
 
@@ -203,7 +214,7 @@ ggsave(
 
 segments_plot <- ggplot(dartmouth_segs) +
   geom_segment(
-    color = "#f07178",
+    color = rose_pink,
     aes(x = x, y = y, xend = xend, yend = yend),
     arrow = arrow(type = "closed", length = unit(0.1, "cm"))
   ) +
@@ -224,22 +235,31 @@ ggsave(
 )
 
 rose_nwbw <- dartmouth |>
-  mutate(color = if_else((row_number()) != 28, "#004B59", "#f07178")) |> 
+  mutate(color = if_else((row_number()) != 28, text_teal, rose_pink)) |> 
   plot_rose("", labels = c("N", "E", "S", "W"), highlight = TRUE) +
-  geom_text(x = 298, y = 26.3, label = "NWbW", color = "#f07178", size = 6)
-rose_nwbw
+  geom_text(x = 298, y = 26.3, label = "NWbW", color = rose_pink, family = my_font, size = 6)
 ggsave(
   file.path(img_dir, "rose_nwbw.png"), 
   rose_nwbw,
   width = 5, height = 5, dpi = 300
 )
 
+rose_nne <- dartmouth |>
+  mutate(color = if_else((row_number()) != 3, text_teal, rose_pink)) |>
+  plot_rose("", labels = c("N", "E", "S", "W"), highlight = TRUE) +
+  geom_text(x = 22, y = 29, label = "NNE", color = rose_pink, family = my_font, size = 6)
+ggsave(
+  file.path(img_dir, "rose_nne.png"), 
+  rose_nne,
+  width = 5, height = 5, dpi = 300
+) 
+
 segments_highlight_nwbw <- dartmouth_segs |>
   mutate(nwbw = group == 28) |>
   ggplot() +
   aes(x = x, y = y, xend = xend, yend = yend, color = nwbw) +
   geom_segment(arrow = arrow(type = "open", length = unit(0.1, "cm"))) +
-  scale_color_manual(values = c("#004B59", "#f07178"), guide = "none") +
+  scale_color_manual(values = c(text_teal, rose_pink), guide = "none") +
   coord_dartmouth +
   bg_transparent() +
   theme(
@@ -257,7 +277,7 @@ segments_highlight_nne <- dartmouth_segs |>
   ggplot() +
   aes(x = x, y = y, xend = xend, yend = yend, color = nne) +
   geom_segment(arrow = arrow(type = "open", length = unit(0.1, "cm"))) +
-  scale_color_manual(values = c("#004B59", "#f07178"), guide = "none") +
+  scale_color_manual(values = c(text_teal, rose_pink), guide = "none") +
   coord_dartmouth +
   bg_transparent() +
   theme(
@@ -268,18 +288,6 @@ ggsave(
   file.path(img_dir, "segments_highlight_nne.png"), 
   segments_highlight_nne,
   width = fig_width, height = fig_height, dpi = 300
-)
-
-
-rose_nne <- dartmouth |>
-  mutate(color = if_else((row_number()) != 3, "#004B59", "#f07178")) |>
-  plot_rose("", labels = c("N", "E", "S", "W"), highlight = TRUE) +
-  geom_text(x = 22, y = 29, label = "NNE", color = "#f07178", size = 6)
-
-ggsave(
-  file.path(img_dir, "rose_nne.png"), 
-  rose_nne,
-  width = 5, height = 5, dpi = 300
 )
 
 all_roses <- cowplot::plot_grid(
@@ -315,27 +323,20 @@ ggsave(
 )
 
 ## Region -----------------------------------------------------------
-ngroups_us <- 16
 
-max_thickness <- 6
-min_thickness <- 0.2
-size_label <- 12
 water_mark <- "OpenSkiStats.org\nLicense: CC BY 4.0"
 region_raw <- arrow::read_parquet(file.path(img_data_dir, "region_roses.parquet"))
+n_ski_areas <- sum(region_raw$ski_areas_count)
 verts <- region_raw |>
   select(region, combined_vertical) |>
   mutate(thickness = sqrt(combined_vertical / max(combined_vertical)) * max_thickness + min_thickness, .keep = "unused") |>
   tibble::deframe()
-n_ski_areas <- sum(region_raw$ski_areas_count)
 
 region <- region_raw |>
   dplyr::select(region, bearings) |>
   tibble::deframe() |>
   lapply(
-    \(x) dplyr::filter(x, num_bins == ngroups_us) |>
-      dplyr::mutate(
-        color = dplyr::if_else(bin_index == 2, "#f07178", "#004B59"),
-      )
+    \(x) dplyr::filter(x, num_bins == ngroups_us)
   )
 
 layout <- c(
@@ -354,6 +355,8 @@ layout <- c(
   area(8, 1), area(8, 3), area(8, 9)
 )
 
+layout_title <- c(area(1, 3, 2, 9), layout)
+
 state_names <- c(
   "AK",
   "ME", "VT", "NH", "WA", "ID", "MT", "ND", "MN", "MI", "NY", "MA", "RI",
@@ -365,97 +368,91 @@ state_names <- c(
 )
 
 state_map <- setNames(state.abb, state.name)
-all_regions <- list(region, state_map[names(region)], vert = verts) |> 
-  purrr::pmap(
-    plot_rose,
-    size_title = size_label,
-    ngr = ngroups_us,
-    type = "us"
+
+for (pal in names(my_aesthetics)) {
+  my_aesthetic <- my_aesthetics[[pal]]
+  all_regions <- list(region, state_map[names(region)], vert = verts) |> 
+    purrr::pmap(
+      plot_rose,
+      size_title = size_label,
+      ngr = ngroups_us,
+      aesthetics = my_aesthetic,
+      type = "us"
+    )
+  
+  names(all_regions) <- state_map[names(region)]
+  
+  state_plots <- lapply(state_names, rose_empty, aesthetics = my_aesthetic) |>
+    setNames(state_names)
+  
+  skiable_states <- names(all_regions)
+  for (i in skiable_states) {
+    state_plots[[i]] <- all_regions[[i]]
+  }
+  state_plots <- state_plots[state_names]
+  
+  title <-
+    ggplot() +
+    theme_void() +
+    coord_cartesian(ylim = c(-2, 0.7)) +
+    annotate(
+      "text",
+      x = 0, y = 0,
+      label = "Which way do you ski?", size = 32,
+      color = my_aesthetic$title_color,
+      family = my_aesthetic$my_font
+    ) +
+    annotate(
+      "text",
+      x = 0, y = -0.9,
+      label = sprintf("Orientations of %s US ski areas", n_ski_areas),
+      size = 18,
+      color = my_aesthetic$title_color,
+      family = my_aesthetic$my_font
+    )
+  
+  map_plots <- c(list(title = title), state_plots) |>
+    wrap_plots(design = layout_title) &
+    plot_annotation(theme = theme(
+      panel.background = element_rect(fill = my_aesthetic$canvas, colour = NA),
+      plot.background = element_rect(fill = my_aesthetic$canvas, colour = NA)
+    ))
+  
+  to_publish <- ggdraw(map_plots) +
+    draw_label(
+      x = 0.985,
+      y = 0.043,
+      size = 28,
+      hjust = 1,
+      color = my_aesthetic$anno_color,
+      fontfamily = my_font,
+      lineheight = 1.6,
+      label = water_mark
+    ) +
+    draw_label(
+      x = 0.165,
+      y = 0.78,
+      size = 28,
+      hjust = 1,
+      color = my_aesthetic$anno_color,
+      fontfamily = my_font,
+      label = "border proportional to\ncombined vertical drop"
+    ) +
+    draw_line(
+      x = c(0.185, 0.21),
+      y = c(0.768, 0.726),
+      color = my_aesthetic$circle_color, size = 2
+    ) +
+    draw_line(
+      x = c(0.17, 0.185),
+      y = c(0.768, 0.768),
+      color = my_aesthetic$circle_color, size = 2
+    )
+  
+  ggsave(
+    file.path(img_dir, sprintf("us_roses_%s.png", pal)),
+    to_publish,
+    width = 11 * 3, height = 8 * 3, units = "in",
+    dpi = 300
   )
-all_regions[[1]]
-names(all_regions) <- state_map[names(region)]
-
-state_plots <- lapply(state_names, rose_empty, size_label = size_label) |>
-  setNames(state_names)
-
-skiable_states <- names(all_regions)
-for (i in skiable_states) {
-  state_plots[[i]] <- all_regions[[i]]
 }
-state_plots <- state_plots[state_names]
-# state_plots[[1]]
-map_plots <- wrap_plots(state_plots, design = layout) &
-  plot_annotation(theme = theme(
-    panel.background = element_rect(fill = "#004B59", colour = NA),
-    plot.background = element_rect(fill = "#004B59", colour = NA)
-  ))
-
-# sysfonts::font_add_google("Nunito Sans")
-# showtext::showtext_auto()
-
-title <-
-  ggplot() +
-  theme_void() +
-  coord_cartesian(ylim = c(-2, 0.7)) +
-  annotate(
-    "text",
-    x = 0, y = 0,
-    label = "Which way do you ski?", size = 32,
-    color = "#EBEBEB",
-    family = my_font
-  ) +
-  annotate(
-    "text",
-    x = 0, y = -0.9,
-    label = sprintf("Orientations of %s US ski areas", n_ski_areas),
-    size = 18,
-    color = "#EBEBEB",
-    family = my_font
-  )
-
-layout_title <- c(area(1, 3, 2, 9), layout)
-map_plots <- c(list(title = title), state_plots) |>
-  wrap_plots(design = layout_title) &
-  plot_annotation(theme = theme(
-    panel.background = element_rect(fill = "#004B59", colour = NA),
-    plot.background = element_rect(fill = "#004B59", colour = NA)
-  ))
-
-to_publish <- ggdraw(map_plots) +
-  draw_label(
-    x = 0.985,
-    y = 0.043,
-    size = 28,
-    hjust = 1,
-    color = "grey60",
-    fontfamily = my_font,
-    lineheight = 1.6,
-    label = water_mark
-  ) +
-  draw_label(
-    x = 0.165,
-    y = 0.78,
-    size = 28,
-    hjust = 1,
-    color = "grey60",
-    fontfamily = my_font,
-    label = "border proportional to\ncombined vertical drop"
-  ) +
-  draw_line(
-    x = c(0.185, 0.21),
-    y = c(0.768, 0.726),
-    color = "grey60", size = 2
-  ) +
-  draw_line(
-    x = c(0.17, 0.185),
-    y = c(0.768, 0.768),
-    color = "grey60", size = 2
-  ) +
-  NULL
-
-ggsave(
-  file.path(img_dir, "us_roses.png"),
-  to_publish,
-  width = 11 * 3, height = 8 * 3, units = "in",
-  dpi = 300
-)
