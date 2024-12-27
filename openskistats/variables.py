2139dc2b0b57591559df1d81599a0f11972b43d8
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2024-12-27 13:39:43 -0500

    _variables.yaml variable management
    for website templating

---
+++ openskistats/variables.py
@@ -0,0 +1,41 @@
+"""
+Variables stored during analysis for website templating usage.
+"""
+
+from pathlib import Path
+from typing import Any
+
+import yaml
+
+from openskistats.utils import get_data_directory
+
+
+def _get_variables_path() -> Path:
+    variables_path = get_data_directory().joinpath("_variables.yaml")
+    if not variables_path.exists():
+        variables_path.touch()
+    return variables_path
+
+
+def _read_variables() -> dict[str, Any]:
+    return yaml.safe_load(stream=_get_variables_path().read_text()) or {}
+
+
+def read_variable(key: str) -> Any:
+    """key supports nested access with __ notation"""
+    variable = _read_variables()
+    for part in key.split("__"):
+        variable = variable[part]
+    return variable
+
+
+def set_variables(**kwargs: Any) -> None:
+    """key supports nested access with __ notation"""
+    variables = _read_variables()
+    for key, value in kwargs.items():
+        parts = key.split("__")
+        variable = variables
+        for part in parts[:-1]:
+            variable = variable.setdefault(part, {})
+        variable[parts[-1]] = value
+    _get_variables_path().write_text(yaml.dump(variables))
